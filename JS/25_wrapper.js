d39bb16a2be4d5219109aed63b3085ae2e76cac5
chare <chare2001@gmail.com>
2019-05-06 13:59:44 +0200

    Wrapper

---
+++ JS/25_wrapper.js
@@ -0,0 +1,35 @@
+function main () {
+    let cadena = 'Hola amigo'
+    let alumno = {
+        saludar:function () {
+
+        }
+    }
+    //let alumno = new Object()
+    let users = ['Pepe', 'Elena', 'Julia']
+    // let users = new Array () 
+    console.log(typeof users)
+    // 
+
+    //cadena.toLowerCase()
+    //EN OTROS LENGUAJES toLowerCase(cadena)
+    
+    console.log(cadena.toLowerCase())
+    //let cadenaTemp = new String()
+    console.log(cadena.length)
+    let aCadena = cadena.split(' ')
+    let cadenaSinEspacios = aCadena.join('')
+
+    cadena = cadena.toUpperCase().split(' ').join('')
+    console.log(cadena)
+
+let url = 'https://www.cife.es/alumnos'
+
+let urlTemp = url.slice(url.indexOf('//')+2)
+urlTemp = urlTemp.slice(0, urlTemp.indexOf('/'))
+console.log(urlTemp)
+console.log(url.split('/' [2]))
+
+}
+
+main()
