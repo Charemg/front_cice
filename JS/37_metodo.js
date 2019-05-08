36dc864c40b2a585ac658f2fda6e7c8c13a02efd
chare <chare2001@gmail.com>
2019-05-08 13:53:28 +0200

    metodo

--- JS/37_metodo.js
+++ JS/37_metodo.js
@@ -10,7 +10,8 @@ let vuelo = {
         claveIATA: 'LAX',
         time:'2004-09-23 10:42',
         ciudad:'Los Ángeles',
-    },toString : function (obj = this) {
+    },
+    toString : function (obj = this) {
     let output = ''
     for (const key in obj) {
         const element = obj[key];
@@ -25,9 +26,24 @@ let vuelo = {
         output = output + `La propiedad ${key} vale ${element}`+ '\n'
         }
     }
-}, mostrarFinal : function () {
+    },
+    mostrar : function () {
     console.log(this.toString())
-}
+    }
 }
 
+vuelo2 = {
+    airline: 'Iberia',
+    number: '185',
+    departure: {
+        claveIATA: 'SYD',
+        time:'2004-09-22 14:55',
+        ciudad:'Sydney',
+    },
+    arrival: {
+        claveIATA: 'LAX',
+        time:'2004-09-23 10:42',
+        ciudad:'Los Ángeles',
+    },
+}
 vuelo.mostrar()
