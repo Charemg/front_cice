8dd1c103c0691cad9d7ad005c47b29dad2fc786d
chare <chare2001@gmail.com>
2019-05-09 13:11:49 +0200

    Herencia

--- JS/39_herencia.js
+++ JS/39_herencia.js
@@ -2,7 +2,48 @@
     //Alumnos
 
 
-    function User (nombre, edad) {
+    function Persona (nombre, edad) {
+       }
+
+    Persona.prototype.saludar = function (aQuien = 'amigo'){
+        console.log(`Hola ${aQuien}, soy ${this.nombre}`)}
+
+    function Alumno(nombre, edad, curso) {
         this.nombre = nombre
         this.edad = edad
+        this.curso = curso
        }
+
+       Alumno.prototype = new Persona()//Hijo prototype = padre
+       Alumno.prototype.constructor = Alumno // Hijo prototype.constructor = hijo
+
+       const alumno1 = new Alumno( 'Pepe', 23, 'JavaScript')
+       const alumno2 = new Alumno( 'Elena', 22, 'JavaScript')
+
+       console.log(alumno1)
+       alumno1.saludar()
+       alumno2.saludar(alumno1.nombre)
+
+
+
+       ////////////////////////////////////////////////////////////////////////
+       
+       // Herencia prototípica (prototypal inheritence)
+       // Basandose en la idea 
+       console.clear
+
+       const person1 = {
+        nombre: 'Pepe',
+        edad: 25,
+        saludar: function (aQuien = 'amigo'){
+            console.log(`Hola ${aQuien}, soy ${this.nombre}`)
+        } 
+    }
+
+    user1 = Object.create(person1)
+    user1.clave = '1234'
+
+    console.log(user1)
+    console.log(user1.nombre)
+    user1.saludar()
+
