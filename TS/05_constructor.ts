4aeac989bc447f89afce7eac39f2a5781e4573ec
chare <chare2001@gmail.com>
2019-06-18 13:37:57 +0200

    Angular

--- TS/05_constructor.ts
+++ TS/05_constructor.ts
@@ -1,16 +1,37 @@
-interface Animal {
+{
+    
+    interface Animal {
     nombre: string
     edad: number
     isDomestic: boolean
     tipo: string
-    comer: () => void
 }
 
-class Perro implements Animal {
-    nombre: string
+interface Herviboro extends Animal {
+    peso: number
+
+}
+
+
+
+class Caballo implements Herbivoro {
+/*     nombre: string
     edad: number
     isDomestic: boolean
     tipo: string
-    colorPiel: string
-    comer () {}
+    peso: number */
+
+    constructor (
+        public nombre: string,
+        public edad: number,
+        public isDomestic: boolean,
+        public tipo: string,
+        public peso: number) {}
+
+
+   }
+   
+   let c1 = new Caballo ('Rufo', 2, true, 'Arabe', 300)
+   console.log(c1)
+
 }
---
+++ angular-cife/src/app/core/footer/footer.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  autor: string;
+  fecha: Date;
+  empresa: string;
+
+  constructor() { 
+    
+
+  }
+
+  ngOnInit() {
+    
+    this.autor = 'Chare Muñoz';
+    this.fecha = new Date();
+    this.empresa = ' CIFE - Fuenlabrada';
+    this.fecha.toLocaleDateString()
+  }
+
+}
